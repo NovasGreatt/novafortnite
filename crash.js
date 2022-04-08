6a1d28cfbb81b4424313695c8f69d65e7f53fdb8
NovasGreattttttt <102698338+NovasGreattttttt@users.noreply.github.com>
2022-04-08 12:24:43 -0500

    Delete crash.js

--- crash.js
+++
@@ -1,8 +0,0 @@
-var kbpsystem = 1;
-function crasher() {
-	do {
-    var x = console.log(kbpsystem);
-    document.getElementById("output").innerHTML = "Crashing your memory....";
-  	kbpsystem = kbpsystem*7;
-		} while (kbpsystem > 77);
-  }
